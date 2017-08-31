756de7b63b92a175fa49363be846cd6f5fd4f684
Michal Scienski <michalscienski@workfront.com>
2017-08-31 04:21:33 -0600

    update

--- scripts/git/push-files-tags.js
+++ scripts/git/push-files-tags.js
@@ -4,19 +4,9 @@ const chalk = require('chalk');
 const exec = require('child-process-promise').exec;
 const catchLogger = require('../helpers/catch-logger');
 const FAILURE_STRATEGIES = require('../helpers/constants').FAILURE_STRATEGIES;
-const getVersionType = require('../helpers/get-version-type');
 const _get = require('lodash/get');
 
 function main() {
-    // OMG what is original?
-    // npm outputs its arguments to process.env
-    // E.g.
-    // $ > npm version minor
-    // process.env.npm_config_argv.original === ["version", "minor"]
-    // TODO figure out a better way to find out what version is being applied to
-    const npmArgs = JSON.parse(process.env.npm_config_argv).original;
-    const isVersioning = npmArgs.indexOf('version') !== -1;
-
     console.log('');
     console.log(chalk.blue('Adding files to commit for versioning'));
     const gitCmd = 'git push --no-verify && git push --tags --no-verify';
